1f1c160f4f3f41f78f30650b24717c5229b13d96
fraud <78248903+alyssagithub@users.noreply.github.com>
2024-05-13 20:13:36 -0700

    Update Course Lists.lua

--- Courses/Course Lists.lua
+++ Courses/Course Lists.lua
@@ -1,7 +1,7 @@
 -- This file is just for managing what courses go in what order, don't mind it
 local CoursesList = {
   "Variables", -- made
-  "Tables",
+  "Tables", -- making
   "Properties",
   "Instance Attributes",
   "Functions",
