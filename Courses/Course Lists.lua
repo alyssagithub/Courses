ec0d4ba24fab01b5b899a4e4ae984b5fe02515ef
fraud <78248903+alyssagithub@users.noreply.github.com>
2024-05-13 20:26:33 -0700

    Update Course Lists.lua

--- Courses/Course Lists.lua
+++ Courses/Course Lists.lua
@@ -1,26 +1,29 @@
 -- This file is just for managing what courses go in what order, don't mind it
 local CoursesList = {
+  -- Top = first things you'll learn
+  -- Bottom = advanced, last things you'll learn
   "Variables", -- made
   "Tables", -- making
-  "Properties",
-  "Instance Attributes",
   "If Statements",
   "Functions",
-  "Data types",
   "Operators",
+  "Properties",
+  "Data types",
   "String Manipulation",
   "Instances",
+  "Instance Attributes",
   "Loops",
-  "Debounce",
   "Ternary Operations",
+  "Debounce",
   "Yielding",
   "Threads",
   "RBXScriptSignals",
   "Print Debugging",
   "Protected Calls",
-  "Raycasting",
   "Module Scripts",
   "Remotes & Bindables",
+  "Raycasting",
+  "Spatial Query",
   "Replication",
   "Type Checking",
   "Metatables",
