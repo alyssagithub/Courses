fa375ce7cf6728f786fc9b606b7c7c6e5fbc6f92
fraud <78248903+alyssagithub@users.noreply.github.com>
2024-05-13 17:54:58 -0700

    Update Course Lists.lua

--- Courses/Course Lists.lua
+++ Courses/Course Lists.lua
@@ -1,6 +1,37 @@
 -- This file is just for managing what courses go in what order, don't mind it
-local CourseLists = {
-  "Variables",
+local CoursesList = {
+  "Variables", -- made
   "Tables",
   "Properties",
+  "Functions",
+  "If Statements",
+  "Data types",
+  "Operators",
+  "String Manipulation",
+  "Instances",
+  "Debounce",
+  "Ternary Operations",
+  "Yielding",
+  "Loops",
+  "Threads",
+  "RBXScriptSignals",
+  "Print Debugging",
+  "Protected Calls",
+  "Raycasting",
+  "Module Scripts",
+  "Remotes & Bindables",
+  "Replication",
+  "Type Checking",
+  "Metatables",
+  "Buffers (never learned these, might not teach)",
+}
+
+local ServicesList = {
+  "Players",
+  "TweenService",
+  "Debris",
+  "CollectionService",
+  "UserInputService",
+  "DataStoreService",
+  "HttpService",
 }
