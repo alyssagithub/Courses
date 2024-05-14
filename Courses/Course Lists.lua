1df9dbaffcead0679a55221b7391a564560ca693
fraud <78248903+alyssagithub@users.noreply.github.com>
2024-05-13 20:53:22 -0700

    Update Course Lists.lua

--- Courses/Course Lists.lua
+++ Courses/Course Lists.lua
@@ -1,41 +1,45 @@
 -- This file is just for managing what courses go in what order, don't mind it
 local CoursesList = {
-  -- Top = first things you'll learn
-  -- Bottom = advanced, last things you'll learn
+  -- Top = easiest to learn/understand
   "Variables", -- made
   "Tables", -- making
   "Operators", -- and, or, ==, >=, etc
   "If Statements",
+  "Print Debugging",
   "Functions",
   "Properties",
-  "Data types",
-  "String Manipulation",
+  "Data Types", -- cframes, vector3/2s, udim2s
+  "String Manipulation", -- concatenation, interpolation, string.format
   "Instances",
-  "Instance Attributes",
-  "Loops",
-  "Ternary Operations",
+  "Attributes",
+  "Loops", -- table & numerical for loop, while loop, repeat loop
+  "Ternary Operations", -- var and var2 or var3, if var then var2 else var3
   "Debounce",
-  "Yielding",
+  "Yielding", -- task.wait()
   "Threads",
-  "RBXScriptSignals",
-  "Print Debugging",
-  "Protected Calls",
+  "RBXScriptSignals", -- :connect(), :once(), :wait(), connection:disconnect()
+  "Protected Calls", -- pcall, xpcall, ypcall
   "Module Scripts",
   "Remotes & Bindables",
   "Raycasting",
-  "Spatial Query",
-  "Replication",
-  "Type Checking",
-  "Metatables",
-  "Buffers", -- never learned these, might not teach
+  "Spatial Query", -- getpartsinpart
+  "Type Checking", -- export type, type, normal usage
+  "Metatables", -- self, methods, oop
+  "Buffers", -- never learned, might not teach
 }
 
 local ServicesList = {
+  -- Top = easiest to learn/understand
+  "Debris",
   "Players",
   "TweenService",
-  "Debris",
   "CollectionService",
   "UserInputService",
+  "ContextActionService",
+  "RunService",
   "DataStoreService",
+  "MarketplaceService",
+  "MemoryStoreService", -- never learned, might not teach
   "HttpService",
+  "EthnicityService",
 }
